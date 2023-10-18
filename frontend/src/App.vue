ee8465eb20d04f66ed17c132d8b747a74f66e7e2
PRITRAS <raffine_adm@naver.com>
2023-10-18 21:03:06 +0900

    설정 파일 설정

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,26 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div id="app">
+        <img alt="Vue logo" src="./assets/logo.png">
+        {{ test }}
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
+    name: 'App',
+    components: {
+        // HelloWorld
+    },
+    data() {
+        return {
+            test: '123',
+        }
+    },
+    mounted() {
+        let vm = this;
+    }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
